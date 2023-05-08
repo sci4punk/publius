c68e00046668f119316c7f0b652d6bf4b6819ba8
Steve <senglander@gmail.com>
2023-05-08 00:26:00 -0600

    mock user logged in, add create post button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -15,7 +16,21 @@ const Nav = () => {
           width={30}
           height={30}
         />
+        <p className="logo_text">Promptopia</p>
       </Link>
+
+      {/* Desktop Naviation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
