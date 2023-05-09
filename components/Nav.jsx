2121c5709da5544b85797ac74a04f9f7a66d88a7
Steve <senglander@gmail.com>
2023-05-09 02:14:23 -0600

    adding dynamic routes, connecting mongo, hitting an error

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,13 +6,13 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
@@ -33,7 +33,7 @@ const Nav = () => {
 
       {/* Desktop Naviation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -68,10 +68,9 @@ const Nav = () => {
           </>
         )}
       </div>
-
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
